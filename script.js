147eddf5ebff5a3043f4bafec6ebc7c2265d84d9
Karisma Yumnam <karismmayumnam@gmail.com>
2026-06-23 15:20:22 -0500

    adding info

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const slides = document.querySelectorAll(".slide");
+    let currentSlide = 0;
+
+    if (slides.length > 0) {
+        slides[0].classList.add("active");
+
+        setInterval(() => {
+            slides[currentSlide].classList.remove("active");
+
+            currentSlide = (currentSlide + 1) % slides.length;
+
+            slides[currentSlide].classList.add("active");
+        }, 3000);
+    }
+});
