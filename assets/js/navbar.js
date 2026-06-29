2a58fd579bd6b214af8b511cb6fbfd86c06a963f
Karisma Yumnam <karismmayumnam@gmail.com>
2026-06-29 15:14:21 -0500

    NA

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,18 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const repoName = ""; 
-    const isGitHubPages = window.location.hostname.includes("github.io");
-
-    const basePath = isGitHubPages ? `/${repoName}` : "";
-
-    fetch(`${basePath}/_includes/navbar.html`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("Navbar not found");
-            }
-            return response.text();
-        })
+    fetch("/_includes/navbar.html")
+        .then(response => response.text())
         .then(data => {
             document.getElementById("navbar").innerHTML = data;
-        })
-        .catch(error => console.error("Navbar loading error:", error));
+        });
 });
